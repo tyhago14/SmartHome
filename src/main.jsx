88b1a0beab3416b0567ad3722151ba5a490b6e90
tyhago14 <tpmarques03@gmail.com>
2023-09-21 20:14:00 +0100

    Github Page 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Login from "./Pages/Login/Login.jsx";
+import Dashboard from "./Pages/Dashboard/Dashboard.jsx";
+import Dispositivos from "./Pages/Dispositivos/Dispositivos.jsx";
+import Conta from "./Pages/Conta/Conta.jsx";
 
+const router = createBrowserRouter([
+    {
+        path: "/smarthome/",
+        element: <App />,
+        children: [
+            {
+                path: "/smarthome/",
+                element: <Login />,
+            },
+            {
+                path: "/smarthome/dashboard",
+                element: <Dashboard />,
+            },
+            {
+                path: "/smarthome/dispositivos",
+                element: <Dispositivos />,
+            },
+            {
+                path: "/smarthome/conta",
+                element: <Conta />,
+            },
+        ],
+    },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
