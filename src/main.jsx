6139550748852d96d611319e2ea464235df61748
tyhago14 <tpmarques03@gmail.com>
2023-09-21 20:51:00 +0100

    v4

--- src/main.jsx
+++ src/main.jsx
@@ -1,43 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Login from "./Pages/Login/Login.jsx";
-import Dashboard from "./Pages/Dashboard/Dashboard.jsx";
-import Dispositivos from "./Pages/Dispositivos/Dispositivos.jsx";
-import Conta from "./Pages/Conta/Conta.jsx";
-
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Login />,
-    },
-    {
-        path: "/smarthome/",
-        element: <App />,
-        children: [
-            {
-                path: "/smarthome/",
-                element: <Login />,
-            },
-            {
-                path: "/smarthome/dashboard",
-                element: <Dashboard />,
-            },
-            {
-                path: "/smarthome/dispositivos",
-                element: <Dispositivos />,
-            },
-            {
-                path: "/smarthome/conta",
-                element: <Conta />,
-            },
-        ],
-    },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <App />
     </React.StrictMode>
 );
