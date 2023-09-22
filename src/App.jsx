4b007bdecec3f583a7e965d1d38e8e51b8ecf6a6
tyhago14 <tpmarques03@gmail.com>
2023-09-22 04:20:22 +0100

    v13

--- src/App.jsx
+++ src/App.jsx
@@ -6,30 +6,26 @@ import Dispositivos from "./Pages/Dispositivos/Dispositivos";
 import Conta from "./Pages/Conta/Conta";
 //import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { GlobalState } from "./GlobalState";
+//import { GlobalState } from "./GlobalState";
 
 function App() {
     return (
         <>
-            <GlobalState>
-                {/*  <ToastContainer /> */}
-                <Router>
-                    <Routes>
-                        <Route exact path="/" element={<Login />} />
-                        <Route
-                            exact
-                            path="/dashboard"
-                            element={<Dashboard />}
-                        />
-                        <Route
-                            exact
-                            path="/dispositivos"
-                            element={<Dispositivos />}
-                        />
-                        <Route exact path="/conta" element={<Conta />} />
-                    </Routes>
-                </Router>
-            </GlobalState>
+            {/* <GlobalState> */}
+            {/*  <ToastContainer /> */}
+            <Router>
+                <Routes>
+                    <Route exact path="/" element={<Login />} />
+                    <Route exact path="/dashboard" element={<Dashboard />} />
+                    <Route
+                        exact
+                        path="/dispositivos"
+                        element={<Dispositivos />}
+                    />
+                    <Route exact path="/conta" element={<Conta />} />
+                </Routes>
+            </Router>
+            {/*   </GlobalState> */}
         </>
     );
 }
--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,16 @@ export default defineConfig({
         port: 8000,
         host: true,
     },
+    build: {
+        outDir: "dist", // Output directory
+        assetsDir: "src/assets", // Directory for assets (e.g., images, fonts)
+        rollupOptions: {
+            input: {
+                Login: "src/Pages/Login/Login.jsx",
+                Dashboard: "src/Pages/Dashboard/Dashboard.jsx",
+                Conta: "src/Pages/Conta/Conta.jsx",
+                Dispositivos: "src/Pages/Dispositivos/Dispositivos.jsx",
+            },
+        },
+    },
 });
