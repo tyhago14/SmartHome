d3571bce824acf7cedd5fe03fa03db70f031fa26
tyhago14 <tpmarques03@gmail.com>
2023-09-22 03:39:35 +0100

    v12

--- vite.config.js
+++ vite.config.js
@@ -4,21 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/SmartHome",
     server: {
         port: 8000,
         host: true,
-    },
-    build: {
-        manifest: true,
-        emptyOutDir: true,
-        rollupOptions: {
-            input: {
-                main: "./src/App.jsx",
-                Pages: "./src/Pages/**/*.jsx",
-            },
-        },
-        // Update the outDir option to point to the root of your GitHub Pages repository
-        outDir: "./public",
     },
 });
