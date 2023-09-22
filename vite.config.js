ba958ffa9eb09ea14b4d38a4b4be6c470ff72f67
tyhago14 <tpmarques03@gmail.com>
2023-09-22 03:32:08 +0100

    v11

--- vite.config.js
+++ vite.config.js
@@ -9,4 +9,16 @@ export default defineConfig({
         port: 8000,
         host: true,
     },
+    build: {
+        manifest: true,
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                main: "./src/App.jsx",
+                Pages: "./src/Pages/**/*.jsx",
+            },
+        },
+        // Update the outDir option to point to the root of your GitHub Pages repository
+        outDir: "./public",
+    },
 });
