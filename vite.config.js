e7f9510f8f677b1629f81787c61d0ff582d85835
tyhago14 <tpmarques03@gmail.com>
2023-09-22 03:07:44 +0100

    V11

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: "/SmartHome",
     server: {
         port: 8000,
         host: true,
