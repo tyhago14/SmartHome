7692eb71413a952d1ace46caf79cb2d71e110cc7
tyhago14 <tpmarques03@gmail.com>
2023-10-02 13:06:35 +0100

    Dispositivos Info

--- src/Pages/Dispositivos/Dispositivos.jsx
+++ src/Pages/Dispositivos/Dispositivos.jsx
@@ -5,6 +5,8 @@ import Sala from "../../assets/sala.png";
 import Cozinha from "../../assets/kitchen.png";
 import Banho from "../../assets/banho.png";
 import Quarto from "../../assets/quarto.png";
+import Bateria from "../../assets/battery.png";
+import Link from "../../assets/link.png";
 
 var divisoes = ["Sala de estar", "Cozinha", "Casa de Banho", "Quarto"];
 var divisoesImg = [Sala, Cozinha, Banho, Quarto];
@@ -12,29 +14,149 @@ var divisoesImg = [Sala, Cozinha, Banho, Quarto];
 const Dispositivos = () => {
     const renderComponent0 = (
         <section className="basic-grid">
-            <div className="gridBox">
-                <div className="gridBox-text">Todos dispositivos</div>
+            <div className="gridBox card-wide2">
+                <div className="gridBox-text">Informações</div>
+                <div className="info">
+                    <div className="info-text">Nome do sensor:</div>
+                    <div className="info-data">Temperatura e Humidade</div>
+                </div>
+                <div className="info">
+                    <div className="info-text">ID do sensor:</div>
+                    <div className="info-data">351642978</div>
+                </div>
+                <div className="info">
+                    <div className="info-text">Broker do sensor:</div>
+                    <div className="info-data">Casa/SalaDeEstar</div>
+                </div>
+            </div>
+            <div className="gridBox card-wide2">
+                <div className="gridBox-text">Bateria</div>
+                <div className="iconValor">
+                    <div className="iconValorLeft">
+                        <img src={Bateria} alt="" />
+                    </div>
+                    <div className="iconValorRight">87%</div>
+                </div>
+            </div>
+            <div className="gridBox card-wide2">
+                <div className="gridBox-text">Ligação</div>
+                <div className="iconValor">
+                    <div className="iconValorLeft">
+                        <img src={Link} alt="" />
+                    </div>
+                    <div className="iconValorRight">99%</div>
+                </div>
             </div>
         </section>
     );
     const renderComponent1 = (
         <section className="basic-grid">
-            <div className="gridBox">
-                <div className="gridBox-text">Todos</div>
+            <div className="gridBox card-wide2">
+                <div className="gridBox-text">Informações</div>
+                <div className="info">
+                    <div className="info-text">Nome do sensor:</div>
+                    <div className="info-data">Gases</div>
+                </div>
+                <div className="info">
+                    <div className="info-text">ID do sensor:</div>
+                    <div className="info-data">98461346</div>
+                </div>
+                <div className="info">
+                    <div className="info-text">Broker do sensor:</div>
+                    <div className="info-data">Casa/Cozinha</div>
+                </div>
+            </div>
+            <div className="gridBox card-wide2">
+                <div className="gridBox-text">Bateria</div>
+                <div className="iconValor">
+                    <div className="iconValorLeft">
+                        <img src={Bateria} alt="" />
+                    </div>
+                    <div className="iconValorRight">46%</div>
+                </div>
+            </div>
+            <div className="gridBox card-wide2">
+                <div className="gridBox-text">Ligação</div>
+                <div className="iconValor">
+                    <div className="iconValorLeft">
+                        <img src={Link} alt="" />
+                    </div>
+                    <div className="iconValorRight">88%</div>
+                </div>
             </div>
         </section>
     );
     const renderComponent2 = (
         <section className="basic-grid">
-            <div className="gridBox">
-                <div className="gridBox-text">Todo2s</div>
+            <div className="gridBox card-wide2">
+                <div className="gridBox-text">Informações</div>
+                <div className="info">
+                    <div className="info-text">Nome do sensor:</div>
+                    <div className="info-data">Switch</div>
+                </div>
+                <div className="info">
+                    <div className="info-text">ID do sensor:</div>
+                    <div className="info-data">1534987</div>
+                </div>
+                <div className="info">
+                    <div className="info-text">Broker do sensor:</div>
+                    <div className="info-data">Casa/CasaDeBanho</div>
+                </div>
+            </div>
+            <div className="gridBox card-wide2">
+                <div className="gridBox-text">Bateria</div>
+                <div className="iconValor">
+                    <div className="iconValorLeft">
+                        <img src={Bateria} alt="" />
+                    </div>
+                    <div className="iconValorRight">48%</div>
+                </div>
+            </div>
+            <div className="gridBox card-wide2">
+                <div className="gridBox-text">Ligação</div>
+                <div className="iconValor">
+                    <div className="iconValorLeft">
+                        <img src={Link} alt="" />
+                    </div>
+                    <div className="iconValorRight">56%</div>
+                </div>
             </div>
         </section>
     );
     const renderComponent3 = (
         <section className="basic-grid">
-            <div className="gridBox">
-                <div className="gridBox-text">Todos3</div>
+            <div className="gridBox card-wide2">
+                <div className="gridBox-text">Informações</div>
+                <div className="info">
+                    <div className="info-text">Nome do sensor:</div>
+                    <div className="info-data">Temperatura e Humidade</div>
+                </div>
+                <div className="info">
+                    <div className="info-text">ID do sensor:</div>
+                    <div className="info-data">32165479</div>
+                </div>
+                <div className="info">
+                    <div className="info-text">Broker do sensor:</div>
+                    <div className="info-data">Casa/Quarto</div>
+                </div>
+            </div>
+            <div className="gridBox card-wide2">
+                <div className="gridBox-text">Bateria</div>
+                <div className="iconValor">
+                    <div className="iconValorLeft">
+                        <img src={Bateria} alt="" />
+                    </div>
+                    <div className="iconValorRight">52%</div>
+                </div>
+            </div>
+            <div className="gridBox card-wide2">
+                <div className="gridBox-text">Ligação</div>
+                <div className="iconValor">
+                    <div className="iconValorLeft">
+                        <img src={Link} alt="" />
+                    </div>
+                    <div className="iconValorRight">70%</div>
+                </div>
             </div>
         </section>
     );
